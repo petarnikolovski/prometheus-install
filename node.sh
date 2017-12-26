c78b4028f6b07425c65a4b3c5a297079cc80220d
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-12-26 13:19:07 +0100

    Refactore node exporter installation script

--- node.sh
+++ node.sh
@@ -2,9 +2,6 @@
 
 # Ubuntu 16.04
 
-# Prometheus installation. It's a lousy script though.
-
-# Example:
 # chmod +x node.sh
 # sudo pwd
 # ./node.sh
@@ -15,21 +12,7 @@ tar xvzf node_exporter-0.15.2.linux-amd64.tar.gz
 sudo cp node_exporter-0.15.2.linux-amd64/node_exporter /usr/local/bin/
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
-# node_exporter.service
-
-echo "[Unit]
-Description=Node Exporter
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-User=node_exporter
-Group=node_exporter
-Type=simple
-ExecStart=/usr/local/bin/node_exporter
-
-[Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/node_exporter.service
+cat ./node/node_exporter.service | sudo tee /etc/systemd/system/node_exporter.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable node_exporter
