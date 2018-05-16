0504a112264ef38e798a7cb632cbdb6d736dbb4c
Petar Nikolovski <petarGitNik@users.noreply.github.com>
2018-05-16 16:34:25 +0200

    Update node exporter version

--- node.sh
+++ node.sh
@@ -4,10 +4,10 @@
 sudo adduser --no-create-home --disabled-login --shell /bin/false --gecos "Node Exporter User" node_exporter
 
 # Download node_exporter and copy utilities to where they should be in the filesystem
-wget https://github.com/prometheus/node_exporter/releases/download/v0.15.2/node_exporter-0.15.2.linux-amd64.tar.gz
-tar xvzf node_exporter-0.15.2.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v0.16.0/node_exporter-0.16.0.linux-amd64.tar.gz
+tar xvzf node_exporter-0.16.0.linux-amd64.tar.gz
 
-sudo cp node_exporter-0.15.2.linux-amd64/node_exporter /usr/local/bin/
+sudo cp node_exporter-0.16.0.linux-amd64/node_exporter /usr/local/bin/
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
 # systemd
@@ -18,5 +18,5 @@ sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
 
 # Installation cleanup
-rm node_exporter-0.15.2.linux-amd64.tar.gz
-rm -rf node_exporter-0.15.2.linux-amd64
+rm node_exporter-0.16.0.linux-amd64.tar.gz
+rm -rf node_exporter-0.16.0.linux-amd64
